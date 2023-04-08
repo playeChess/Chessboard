a0adb496dffd603249e0eae0cf24a7e74251683f
Jasiukiewicz Tymon <JasiukiewiczTymon@outlook.fr>
2023-04-08 23:25:42 +0200

    Create Chessboard.stories.js

---
+++ src/Chessboard.stories.js
@@ -0,0 +1,14 @@
+import { Chessboard } from './Chessboard';
+
+export default {
+  title: 'Chessboard',
+  component: Chessboard,
+  tags: ['autodocs'],
+};
+
+export const Primary = {
+  args: {
+    primary: true,
+    label: 'Button',
+  },
+};
